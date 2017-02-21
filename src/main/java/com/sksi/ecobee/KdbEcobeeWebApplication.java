27c39cdddb70fc60ce882218e3bbb7c477a5a340
Kevin Brown <silentkevin@gmail.com>
2017-02-21 07:50:05 -0500

    stuff

---
+++ src/main/java/com/sksi/ecobee/KdbEcobeeWebApplication.java
@@ -0,0 +1,12 @@
+package com.sksi.ecobee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdbEcobeeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdbEcobeeWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sksi/ecobee/KdbEcobeeWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sksi.ecobee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KdbEcobeeWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
