e93a7c29d7ff12becde121b9c726d4f9dfd95b9f
Kevin Brown <silentkevin@gmail.com>
2017-02-21 17:56:37 -0500

    checkpoint

--- src/main/groovy/com/sksi/ecobee/config/SecurityConfig.java
+++ src/main/groovy/com/sksi/ecobee/config/SecurityConfig.java
@@ -1,18 +1,16 @@
 package com.sksi.ecobee.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import groovy.transform.TypeChecked;
-import groovy.transform.TypeCheckingMode;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
-    @TypeChecked(TypeCheckingMode.SKIP)
     protected void configure(HttpSecurity http) throws Exception {
-        String[] permitAllExpression = {"/*", "/**"};
+        String[] permitAllExpression = {"/", "/home", "/hi"};
 //        http
 //                .authorizeRequests()
 //                .antMatchers(permitAllExpression)
@@ -21,6 +19,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .authorizeRequests()
 //                .anyRequest()
 //                .authenticated();
-        http.authorizeRequests().antMatchers(permitAllExpression).permitAll();
+//        http.authorizeRequests().antMatchers(permitAllExpression).permitAll();
+        http
+                .authorizeRequests()
+                .antMatchers(permitAllExpression).permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+               .withUser("user").password("password").roles("USER");
     }
 }
