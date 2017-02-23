acf03e0a996234ab25ccccc961e127c2e3e1b31d
Kevin Brown <silentkevin@gmail.com>
2017-02-23 08:27:41 -0500

    trying webserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-var path = require('path');
+var path = require("path");
+var webpack = require('webpack');
+var nodeModulesPath = path.join(__dirname, 'node_modules');
+var Dashboard = require('webpack-dashboard');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+var dashboard = new Dashboard();
 
 module.exports = {
     entry: './src/main/js/app.js',
@@ -9,6 +14,12 @@ module.exports = {
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors-bundle.js'),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new DashboardPlugin(dashboard.setData)
+    ],
     module: {
         loaders: [
             {
