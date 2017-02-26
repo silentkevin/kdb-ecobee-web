2c07838108e4adc2578e7fb446b2c563d3878911
Kevin Brown <silentkevin@gmail.com>
2017-02-26 11:39:00 -0500

    checkpoint

--- src/main/groovy/com/sksi/ecobee/config/HerokuConfig.groovy
+++ src/main/groovy/com/sksi/ecobee/config/HerokuConfig.groovy
@@ -1,8 +1,10 @@
 package com.sksi.ecobee.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.web.client.RestTemplate
 
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
@@ -14,6 +16,8 @@ import javax.annotation.PostConstruct
 @CompileStatic
 @Slf4j
 class HerokuConfig {
+    @Autowired RestTemplate restTemplate
+
     @PostConstruct
     void init() {
         log.info("HEROKU IS ACTIVE")
@@ -22,5 +26,6 @@ class HerokuConfig {
     @Scheduled(initialDelay = 30_000L, fixedRate = 30_000L)
     void doSomethingScheduled() {
         log.info("**************** SCHEDULED ****************")
+        restTemplate.getForObject("https://kdb-ecobee-web.herokuapp.com/", Map.class)
     }
 }
